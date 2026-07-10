99db9a0be09f3b5fde019f5035b4b9f478e9aa6b
Ravi V <gvrbinternational@gmail.com>
2026-07-10 22:42:53 +0530

    Refine Featured Product feature list: icon sizing, mobile width, spacing

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('explore-btn').addEventListener('click', function () {
+    document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('learn-more-btn').addEventListener('click', function () {
+    document.getElementById('why-cocojag').scrollIntoView({ behavior: 'smooth' });
+});
